6f7b7e58a17be4a9ed4f7a962c3c9713437276cc
Grace <helen.g.anderson@me.com>
2022-04-04 20:00:43 +1000

    youtube call seems to be getting better results and handling 'no viewo' error ok

--- assets/script.js
+++ assets/script.js
@@ -79,94 +79,23 @@ var getYouTubeVideo = function (foundHero) {
   var videoHero = foundHero;
   console.log("videoHero", videoHero);
 
-  //MARVEL - UPDATED URL TO INCLUDE MARVEL CHANNEL AND UPLOAD VIDEO PLAYLIST
-  //Marvel call - updated url to include Marvel channel's upload video playlist
-  //this is supposed to ensure that only marvel videos are selected by the search
-
+  // request to Marvel api
   var videoRequestUrl =
-    "https://www.googleapis.com/youtube/v3/search?order=title&key=" +
-    youTubeApiKey +
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet" +
     //marvel's channel id
     "&channelId=UCvC4D8onUfXzvjTOM-dBfEA" +
-    //marvel's upload playlist
-    "&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&part=snippet,id&order=date&maxResults=" +
+    //type video
+    "&type=video&maxResults=" +
+    //max results = 1
     youTubeMaxResults +
+    //search for search term
     "&q=" +
-    videoHero;
-  ////////////////////////////////
-
-  // MARVEL
-  // this is how to find Marvel's upload playlist ID
-  // https://www.googleapis.com/youtube/v3/channels?&part=contentDetails&forUsername=MARVEL&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0
-  // results
-  // {
-  // "kind": "youtube#channelListResponse",
-  // "etag": "vqnTHvEI4hsi6iyr6-KOWaUShBs",
-  // "pageInfo": {
-  // "totalResults": 1,
-  // "resultsPerPage": 5
-  // },
-  // "items": [
-  // {
-  // "kind": "youtube#channel",
-  // "etag": "14j1dvPAR2VCOzS2QX3SqDeI7nE",
-  // "id": "UCvC4D8onUfXzvjTOM-dBfEA",
-  // "contentDetails": {
-  // "relatedPlaylists": {
-  // "likes": "",
-  // "uploads": "UUvC4D8onUfXzvjTOM-dBfEA"
-  // UPLOADS is PLAYLIST ID
-  // }
-  // }
-  // }
-  // ]
-  // }
-
-  //   var videoRequestUrl =
-  //   "https://www.googleapis.com/youtube/v3/channels?&part=contentDetails&forUserName=GoogleDevelopers&key=" + youTubeApiKey;
-  //   "https://www.googleapis.com/youtube/v3/channels?&part=contentDetails&forUserName=MARVEL&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0"
-  //NB UserName is legacy, replaced by channel ID
-  //returns playlist that corresponds to the uploads for the channel -> "uploads" ID => suggestion is to save "uploads" local variable and use in follow up call
-  //follow up call is for youTube playlist items list
-  //for call to playlist uses snippet
-  //part=snippet
-  //playlistId=["uploads" playlist ID] - got from above call to channel
-  //also set max results to (e.g) 50 (max you ca )
-  //from video Getting a Channel's uploads in v3 (GoogleDevelopers) - 8.15
-  // "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0&q=spider-man"
-
-  // "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&q=spider-man&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0"
-
-  //www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0
-
-  //TEST THIS BELOW
-  //updated to try to use marvelplay list and search term
-  //CANNOT TEST as OVER QUOTA LIMIT
-  //https://www.googleapis.com/youtube/v3/search?key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&part=snippet,id&order=date&maxResults=1&q=moon
-
-  //var videoRequestUrl =
-  //     "https://www.googleapis.com/youtube/v3/search?key=" +
-  //     youTubeApiKey +
-  //     "&playlistId=UUvC4D8onUfXzvjTOM-dBfEA" +
-  //     "&part=snippet,id&order=date&maxResults=" +
-  //     youTubeMaxResults +
-  //     "&q=" +
-  //     videoHero;
-
-  // playlistItems?part=snippet&maxResults=50&playlistId=UUvC4D8onUfXzvjTOM-dBfEA
-
-  //   search?key=" +
-
-  //   "&channelId=UCvC4D8onUfXzvjTOM-dBfEA" +
-  //   "&part=snippet,id&order=date&maxResults=" +
-  //   youTubeMaxResults +
-  //   "&q=" +
-  //   videoHero;
-
-  ////////////////////
+    videoHero +
+    "&key=" +
+    youTubeApiKey;
 
   // calling the youTube API to search for one video with search word "videoHero"
-  https: fetch(videoRequestUrl).then(function (response) {
+  fetch(videoRequestUrl).then(function (response) {
     response.json().then(function (data) {
       var videoHeroSearch = data;
       console.log("videoHeroSearch - data", videoHeroSearch);
@@ -179,7 +108,7 @@ var getYouTubeVideo = function (foundHero) {
       } else {
         marvelHeroEl.textContent = "Sorry no hero video found";
         noResultsModal(".modal-wrapper", ".modal-content", true);
-      } 
+      }
     });
   });
 };
@@ -213,25 +142,20 @@ searchFormEl.addEventListener("submit", formSubmitHandler);
 
 // MODAL FUNCTION
 function noResultsModal(
-//   openModalSelector,
   modalWrapperSelector,
   modalContentSelector,
   closeModal = false
 ) {
   //select the elements
-//   const openModalBtn = document.querySelector(openModalSelector);
   const modalWrapperElement = document.querySelector(modalWrapperSelector);
   const modalContentElement = document.querySelector(modalContentSelector);
 
   // style the modal elements
-//   openModalBtn.classList.add("open-modal");
   modalWrapperElement.classList.add("modal-wrapper");
   modalContentElement.classList.add("modal-content");
 
-  //open modal onClick (event listener)
-//   openModalBtn.addEventListener("click", () => {
-    modalWrapperElement.style.display = "block";
-//   });
+  //update display from none to block
+  modalWrapperElement.style.display = "block";
 
   //add X button to close modal
   if (closeModal) {
@@ -252,6 +176,3 @@ function noResultsModal(
   });
 }
 
-// call the modal function
-// pass modal classes
-// modal("#open-modal", ".modal-wrapper", ".modal-content", true);
