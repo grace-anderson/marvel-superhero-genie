a358b1a922995aab33684cab5aacfefbcf58470c
Grace <helen.g.anderson@me.com>
2022-04-04 18:18:11 +1000

    Updated files with modal

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var searchFormEl = document.querySelector("#search-form");
 var queryEl = document.querySelector("#query-name");
 var marvelHeroEl = document.querySelector("#marvel-hero-body");
+var searchButton = document.querySelector("#search-button");
 //youTube API variables
 const youTubeApiKey = "AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0";
 const youTubeMaxResults = "1";
@@ -54,6 +55,7 @@ var getHeroRepos = function (hero) {
         displayHero(foundHero);
       } else if (heroSearch === undefined) {
         marvelHeroEl.textContent = "Sorry no heroes found";
+        noResultsModal(".modal-wrapper", ".modal-content", true);
       }
     });
   });
@@ -62,6 +64,7 @@ var getHeroRepos = function (hero) {
 var displayHero = function (foundHero) {
   if (foundHero.length === 0) {
     marvelHeroEl.textContent = "No hero found";
+    noResultsModal(".modal-wrapper", ".modal-content", true);
     return;
   }
   marvelHeroEl.textContent = "Showing results for " + foundHero;
@@ -71,11 +74,12 @@ var displayHero = function (foundHero) {
   console.log("foundHero", foundHero);
 };
 
-//search for youTube video data using hero search term, return data for one video
+//search for youTube Marvel channel videos using hero search term, return data for one video
 var getYouTubeVideo = function (foundHero) {
   var videoHero = foundHero;
   console.log("videoHero", videoHero);
-  //UPDATE URL TO INCLUDE MARVEL CHANNEL
+
+  //***ORIGINAL*** UPDATE URL TO INCLUDE MARVEL CHANNEL
   var videoRequestUrl =
     "https://www.googleapis.com/youtube/v3/search?key=" +
     youTubeApiKey +
@@ -84,9 +88,81 @@ var getYouTubeVideo = function (foundHero) {
     youTubeMaxResults +
     "&q=" +
     videoHero;
+  // "&type=video" = call is not filtering data to receive only type=video;
+  ////////////////////////////////
+
+  // MARVEL
+  // this is how to find Marvel's upload playlist ID
+  // https://www.googleapis.com/youtube/v3/channels?&part=contentDetails&forUsername=MARVEL&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0
+  // results
+  // {
+  // "kind": "youtube#channelListResponse",
+  // "etag": "vqnTHvEI4hsi6iyr6-KOWaUShBs",
+  // "pageInfo": {
+  // "totalResults": 1,
+  // "resultsPerPage": 5
+  // },
+  // "items": [
+  // {
+  // "kind": "youtube#channel",
+  // "etag": "14j1dvPAR2VCOzS2QX3SqDeI7nE",
+  // "id": "UCvC4D8onUfXzvjTOM-dBfEA",
+  // "contentDetails": {
+  // "relatedPlaylists": {
+  // "likes": "",
+  // "uploads": "UUvC4D8onUfXzvjTOM-dBfEA"
+  // UPLOADS is PLAYLIST ID
+  // }
+  // }
+  // }
+  // ]
+  // }
+
+  //   var videoRequestUrl =
+  //   "https://www.googleapis.com/youtube/v3/channels?&part=contentDetails&forUserName=GoogleDevelopers&key=" + youTubeApiKey;
+  //   "https://www.googleapis.com/youtube/v3/channels?&part=contentDetails&forUserName=MARVEL&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0"
+  //NB UserName is legacy, replaced by channel ID
+  //returns playlist that corresponds to the uploads for the channel -> "uploads" ID => suggestion is to save "uploads" local variable and use in follow up call
+  //follow up call is for youTube playlist items list
+  //for call to playlist uses snippet
+  //part=snippet
+  //playlistId=["uploads" playlist ID] - got from above call to channel
+  //also set max results to (e.g) 50 (max you ca )
+  //from video Getting a Channel's uploads in v3 (GoogleDevelopers) - 8.15
+  // "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0&q=spider-man"
+
+  // "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&q=spider-man&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0"
+
+  //www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0
+
+  //TEST THIS BELOW
+  //updated to try to use marvelplay list and search term
+  //CANNOT TEST as OVER QUOTA LIMIT
+  //https://www.googleapis.com/youtube/v3/search?key=AIzaSyAfUF4iIR3SGaR4Zp32vLIHhtUBJH2nPR0&playlistId=UUvC4D8onUfXzvjTOM-dBfEA&part=snippet,id&order=date&maxResults=1&q=spider-man
+
+  //var videoRequestUrl =
+  //     "https://www.googleapis.com/youtube/v3/search?key=" +
+  //     youTubeApiKey +
+  //     "&playlistId=UUvC4D8onUfXzvjTOM-dBfEA" +
+  //     "&part=snippet,id&order=date&maxResults=" +
+  //     youTubeMaxResults +
+  //     "&q=" +
+  //     videoHero;
+
+  // playlistItems?part=snippet&maxResults=50&playlistId=UUvC4D8onUfXzvjTOM-dBfEA
+
+  //   search?key=" +
+
+  //   "&channelId=UCvC4D8onUfXzvjTOM-dBfEA" +
+  //   "&part=snippet,id&order=date&maxResults=" +
+  //   youTubeMaxResults +
+  //   "&q=" +
+  //   videoHero;
+
+  ////////////////////
 
   // calling the youTube API to search for one video with search word "videoHero"
-  fetch(videoRequestUrl).then(function (response) {
+  https: fetch(videoRequestUrl).then(function (response) {
     response.json().then(function (data) {
       var videoHeroSearch = data;
       console.log("videoHeroSearch - data", videoHeroSearch);
@@ -98,6 +174,7 @@ var getYouTubeVideo = function (foundHero) {
         displayHeroVideo(heroVideoId);
       } else if (videoHeroSearch === undefined) {
         marvelHeroEl.textContent = "Sorry no hero video found";
+        noResultsModal(".modal-wrapper", ".modal-content", true);
       }
     });
   });
@@ -110,13 +187,12 @@ var displayHeroVideo = function (heroVideoId) {
     return;
   }
   //create url for hero video
-  //   var heroVideoUrl = "https://www.youtube.com/watch?v=" + heroVideoId;
   var heroVideoUrl = "https://www.youtube.com/embed/" + heroVideoId;
   console.log("video link for hero video: ", heroVideoUrl);
 
   //append elements to display video
   var videoContainerEl = document.createElement("div");
-  videoContainerEl.classList.add("video-container");
+  videoContainerEl.classList.add("video-container", "scale-in");
 
   var videoFrameEl = document.createElement("iframe");
   videoFrameEl.width = "853";
@@ -130,3 +206,48 @@ var displayHeroVideo = function (heroVideoId) {
 };
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
+
+// MODAL FUNCTION
+function noResultsModal(
+//   openModalSelector,
+  modalWrapperSelector,
+  modalContentSelector,
+  closeModal = false
+) {
+  //select the elements
+//   const openModalBtn = document.querySelector(openModalSelector);
+  const modalWrapperElement = document.querySelector(modalWrapperSelector);
+  const modalContentElement = document.querySelector(modalContentSelector);
+
+  // style the modal elements
+//   openModalBtn.classList.add("open-modal");
+  modalWrapperElement.classList.add("modal-wrapper");
+  modalContentElement.classList.add("modal-content");
+
+  //open modal onClick (event listener)
+//   openModalBtn.addEventListener("click", () => {
+    modalWrapperElement.style.display = "block";
+//   });
+
+  //add X button to close modal
+  if (closeModal) {
+    modalContentElement.innerHTML += "<span class='close-modal'>&times;</span>";
+
+    const closeModalBtn = modalContentElement.querySelector(".close-modal");
+
+    closeModalBtn.addEventListener("click", () => {
+      modalWrapperElement.style.display = "none";
+    });
+  }
+
+  //close modal if click outside of the modal
+  modalWrapperElement.addEventListener("click", (event) => {
+    if (event.target === modalWrapperElement) {
+      modalWrapperElement.style.display = "none";
+    }
+  });
+}
+
+// call the modal function
+// pass modal classes
+// modal("#open-modal", ".modal-wrapper", ".modal-content", true);
